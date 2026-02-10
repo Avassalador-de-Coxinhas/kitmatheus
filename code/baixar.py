46512658ddb2623db10e07bb774753735bbad548
MatheusSR <matheusrivieira@usp.br>
2026-02-10 09:26:01 -0300

    Atualização de dados Cria script Python para baixar dados, de temperatura media em 225 paises. Dezip automaticamente, para facilitar a análise.

---
+++ code/baixar.py
@@ -0,0 +1,10 @@
+# baixa os dados de ºC
+
+import pooch
+
+pooch.retrieve(
+ url="https://github.com/compgeolab/temperature-data/releases/download/2025-02-11/temperature-data.zip", 
+ known_hash="md5:d102212049af1695b686c94ae1eea233",
+ path="../data",
+ processor=pooch.Unzip(extract_dir="temperature"),
+)
